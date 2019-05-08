f50b7e8761c27f3d78c095b9f26cb661ab58894f
GermanRodrickson <germanrodrickson@gmail.com>
2019-05-08 23:02:59 +0200

    card

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 
-function App() {
-  const [data, setData] = useState({});
+import Card from './Card';
+import { kelvinToCelsius } from '../helpers/';
+
+const App = () => {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
+    const fetchData = async () => {
       const response = await fetch(
         'https://raw.githubusercontent.com/Influencity/job-offers/master/coding-test/weather-api-2019-04/almeria.json'
       );
       const data = await response.json();
-      setData(data.list);
-    }
+      const [item] = data.list;
+      setData(item);
+    };
 
     fetchData();
   }, []);
 
   return (
     <section>
-      <h1>hi</h1>
+      <Card name={data.name} temp={kelvinToCelsius()} />
     </section>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  width: 20%;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+`;
+
+const Title = styled.h2`
+  display: block;
+  text-align: center;
+`;
+
+const Temperature = styled.strong`
+  display: block;
+  text-align: center;
+`;
+
+export default props => {
+  return (
+    <Wrapper>
+      <Title>{props.name}</Title>
+      <Temperature>16 ºC</Temperature>
+    </Wrapper>
+  );
+};
